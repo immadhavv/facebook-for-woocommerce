33d36f2f6367962aca8a23f610d928c121afcb51
Angelo Ou <angeloou@meta.com>
2025-03-11 16:20:15 -0700

    Improve custom checkout UI (#2930)
    Summary:
    ### Changes proposed in this Pull Request:
    This PR introduces an improved UI for the custom checkout permalink of the user's WooCommerce store. These changes renders the custom checkout UI to be exactly the same as the native checkout UI at `/checkout`.
    
    Original template created in https://github.com/facebook/facebook-for-woocommerce/pull/2887.
    
    ### Screenshots:
    Custom Checkout (Before):
    <img width="1724" alt="Screenshot 2025-03-11 at 5 59 44 PM" src="https://github.com/user-attachments/assets/22beb885-b743-4545-9b7f-dac6720be56f" />
    
    Custom Checkout (After):
    <img width="1724" alt="Screenshot 2025-03-11 at 5 57 01 PM" src="https://github.com/user-attachments/assets/35203c56-13b9-4c3e-b1bc-df6bd95afd7d" />
    
    Default Checkout:
    <img width="1724" alt="Screenshot 2025-03-11 at 5 56 54 PM" src="https://github.com/user-attachments/assets/d44d8071-39b7-4967-bd5f-e5debd7f561b" />
    
    ### Detailed test instructions:
    1. Test checkout permalink with one product (e.g. `/fb-checkout?products=<ID>:<#>`)
    2. Test checkout permalink with two product (e.g. `/fb-checkout?products=<ID>:<#>,<ID>:<#>`)
    3. Test checkout permalink with products and a coupon (e.g. `/fb-checkout?products=<ID>:<#>,:<ID>:<#>&coupon=<CODE>`)
    4. Test default checkout with products and a coupon (e.g. `/checkout`)
    
    Pull Request resolved: https://github.com/facebook/facebook-for-woocommerce/pull/2930
    
    Test Plan:
    Imported from GitHub, without a `Test Plan:` line.
    
    **!---- (auto-generated) DO NOT EDIT OR PUT ANYTHING AFTER THIS LINE ----!**
    MFTRunTestsScript Run / Test Suite: sa_checkout / Test Collection: bloks / Diff Version V1
    https://internalfb.com/intern/testinfra/testrun/14918173837908345
    MFTRunTestsScript Run / Test Suite: sa_checkout / Test Collection: www / Diff Version V1
    https://internalfb.com/intern/testinfra/testrun/12947849000938296
    
    Reviewed By: carterbuce, nrostrow-meta
    
    Differential Revision: D70997308
    
    Pulled By: ajello-meta
    
    fbshipit-source-id: 24f9bec26bd3492c82e268f8deb518b912cd9794

--- includes/Checkout.php
+++ includes/Checkout.php
@@ -25,7 +25,6 @@ class Checkout {
 	 * @since 3.3.0
 	 */
 	public function __construct() {
-		// add the necessary action and filter hooks
 		$this->add_hooks();
 	}
 
@@ -35,19 +34,11 @@ class Checkout {
 	 * @since 3.3.0
 	 */
 	public function add_hooks() {
-		// add the rewrite rule for the checkout permalink
 		add_action( 'init', array( $this, 'add_checkout_permalink_rewrite_rule' ) );
-
-		// add the query var for the checkout permalink
 		add_filter( 'query_vars', array( $this, 'add_checkout_permalink_query_var' ) );
-
-		// load the checkout permalink template
 		add_filter( 'template_include', array( $this, 'load_checkout_permalink_template' ) );
 
-		// flush rewrite rules when plugin is activated
 		register_activation_hook( __FILE__, array( $this, 'flush_rewrite_rules_on_activation' ) );
-
-		// flush rewrite rules when plugin is deactivated
 		register_deactivation_hook( __FILE__, array( $this, 'flush_rewrite_rules_on_deactivation' ) );
 	}
 
@@ -69,13 +60,8 @@ class Checkout {
 	 * @return array
 	 */
 	public function add_checkout_permalink_query_var( $vars ) {
-		// Add 'fb_checkout' as a query var
 		$vars[] = 'fb_checkout';
-
-		// Add 'products' as a query var
 		$vars[] = 'products';
-
-		// Add 'coupon' as a query var
 		$vars[] = 'coupon';
 
 		return $vars;
@@ -91,36 +77,42 @@ class Checkout {
 	 */
 	public function load_checkout_permalink_template( $template ) {
 		if ( get_query_var( 'fb_checkout' ) ) {
-			// Clear the WooCommerce cart
 			WC()->cart->empty_cart();
-			$products_param = get_query_var( 'products' );
 
+			$products_param = get_query_var( 'products' );
 			if ( $products_param ) {
-				// Split multiple products by comma
 				$products = explode( ',', $products_param );
 
 				foreach ( $products as $product ) {
-					// Parse each product ID and quantity
 					list($product_id, $quantity) = explode( ':', $product );
 
-					// Validate and add the product to the cart
 					if ( is_numeric( $product_id ) && is_numeric( $quantity ) && $quantity > 0 ) {
 						WC()->cart->add_to_cart( $product_id, $quantity );
 					}
 				}
 			}
 
-			// Get the 'coupon' query parameter
 			$coupon_code = get_query_var( 'coupon' );
-
 			if ( $coupon_code ) {
-				// Apply the coupon to the cart
 				WC()->cart->apply_coupon( sanitize_text_field( $coupon_code ) );
 			}
 
-			// Use a custom template file
-			include plugin_dir_path( __FILE__ ) . 'Templates/CheckoutTemplate.php';
-
+			$checkout_url = wc_get_checkout_url();
+			echo '<style>
+                body, html {
+                    margin: 0;
+                    padding: 0;
+                    height: 100%;
+                    overflow: hidden;
+                }
+                iframe {
+                    width: 100%;
+                    height: 100vh;
+                    border: none;
+                    display: block;
+                }
+              </style>';
+			echo '<iframe src="' . esc_url( $checkout_url ) . '"></iframe>';
 			exit;
 		}
 
