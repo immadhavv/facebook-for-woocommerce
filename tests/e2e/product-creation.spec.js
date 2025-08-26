3b5fd83de2f99ef17b45ba15a01204b2720eec78
Niranjan Madhavan Dindukurthu <nmadhav@meta.com>
2025-08-26 18:41:47 +0100

    updated test id

--- tests/e2e/product-creation.spec.js
+++ tests/e2e/product-creation.spec.js
@@ -112,7 +112,7 @@ test.describe('Facebook for WooCommerce - Product Creation E2E Tests', () => {
       await page.waitForSelector('#title', { timeout: 120000 });
 
       // Fill product details
-      await page.fill('#title', 'Test Simple Product - E2E 1');
+      await page.fill('#title', 'Test Simple Product - E2E 2');
 
       // Try to add content - handle different editor types
       try {
